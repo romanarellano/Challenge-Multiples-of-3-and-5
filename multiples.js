c87447fbf35079eec8e703f036839b090d29c257
romanarellano <romanarellano@yahoo.com>
2015-01-14 20:11:07 -1000

    no_for_loops

--- multiples.js
+++ multiples.js
@@ -1,18 +1,19 @@
-/**
+ /**
  * sumOfAMultiple() sums of the multiples of 3 and 5 for any number passed in
  * @param  {Number} n Number that is to be used for calculating multiples up to that Number
  * @return {Number}
  */
 exports.sumOfAMultiple = function(n){
   var sum = 0;
- var num=0;
-while(num<n){
+  var num = 0;
+  while(num<n){
 
-  if(num%3===0 || numm%5===0){
-    sum+=num;
+    if(num % 3===0 || num % 5 === 0){
+      sum += num;
 
+    }
+    num++;
   }
-}
 
 
   
